eff79922ac1744321c1c4313c3eef38b681e736b
Jakub Musialowski <mds@mds.biz.pl>
2024-11-16 17:02:54 +0100

    Basic search bar styling

--- src/common/SearchBar/index.jsx
+++ src/common/SearchBar/index.jsx
@@ -1,7 +1,13 @@
+import { BiSearch } from "react-icons/bi";
+
 const SearchBar = () => (
-    <form action="">
-        <input type="text" />
-        <button>Sz</button>
+    <form action="" className="flex justify-end items-center gap-2">
+        <input className="p-[4px] text-[14px]/[1] text-primaryTextColor placeholder:text-[14px]/[1]" 
+            type="text"
+            placeholder="Wyszukaj zadanie"  />
+        <button className="appearance-none p-1 bg-borderGray border rounded-lg border-borderGray transition easy-in duration-700 hover:bg-secondaryText">
+            <BiSearch />
+        </button>
     </form>
 );
 
