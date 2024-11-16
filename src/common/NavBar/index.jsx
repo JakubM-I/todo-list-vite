9eadb2ffc40074e4a3616120f3fbd1d3eacf39c5
Jakub Musialowski <mds@mds.biz.pl>
2024-11-16 12:26:21 +0100

    Menu container center

--- src/common/NavBar/index.jsx
+++ src/common/NavBar/index.jsx
@@ -2,12 +2,14 @@ import logo from "../../assets/done-icon.png"
 import SearchBar from "../SearchBar";
 
 const NavBar = () => (
-    <div className="p-3 bg-primaryGreyColor mb-8 flex justify-center items-center">
+    <div className="p-3 bg-primaryGreyColor mb-8 flex justify-center items-center relative">
         <div className="flex gap-2 justify-start items-center mr-auto">
             <img src={logo} alt="TaskApp" />
             <h1 className="font-bold text-[24px]/[1] text-black">TaskApp</h1>
         </div>
-        <p>Menu</p>
+        <div className="absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]">
+            <p>Menu</p>
+        </div>
         <div className="ml-auto">
             <SearchBar />
         </div>
