dade0e94ec1506371723426d73ae78af5025ec16
Jakub Musialowski <mds@mds.biz.pl>
2024-11-16 19:35:01 +0100

    Icon styling on tasks list corrected

--- src/features/tasks/TaskList/index.jsx
+++ src/features/tasks/TaskList/index.jsx
@@ -1,3 +1,4 @@
+import { IconContext } from "react-icons";
 import { BiCheckCircle } from "react-icons/bi";
 import { BiTrash } from "react-icons/bi";
 
@@ -6,24 +7,24 @@ const TasksList = () => {
     return (
         <ul>
             <li className="flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid">
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-doneButton">
+                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-doneButton flex justify-center items-center text-[#fff] text-[20px]">
                     <BiCheckCircle />
                 </button>
                     <div className="grow">
                     Zadanie 1
                     </div>
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-removeButton">
+                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-removeButton flex justify-center items-center text-[20px]">
                     <BiTrash />
                 </button>
             </li>
             <li className="flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid">
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-doneButton">
+                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-doneButton flex justify-center items-center  text-[#fff] text-[20px]">
                     <BiCheckCircle />
                 </button>
                     <div className="grow">
                     Zadanie 2
                     </div>
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-removeButton">
+                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-removeButton flex justify-center items-center text-[20px]">
                     <BiTrash />
                 </button>
             </li>
