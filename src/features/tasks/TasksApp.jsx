698e14e239d5bd7c0aa8401d407620a936955234
Jakub Musialowski <mds@mds.biz.pl>
2024-11-16 18:01:16 +0100

    Tasks list component added with basic styling

--- src/features/tasks/TasksApp.jsx
+++ src/features/tasks/TasksApp.jsx
@@ -1,20 +1,21 @@
 import PageHeader from "../../common/Header";
 import PageSection from "../../common/PageSection";
 import AddMenu from "./AddMenu";
+import TasksList from "./TaskList";
 
 const TasksApp = () => {
 
     return (
     <>
-       <PageHeader title="Lista zadań" />
-       <PageSection 
+       {/* <PageHeader title="Lista zadań" /> */}
+       {/* <PageSection 
             title="Dodaj zadanie"
             body={<p>ToDo list - Will be here soon</p>}
-       />
+       /> */}
         <PageSection 
             title="Lista zadań"
             menu={<AddMenu />}
-            body={<p>ToDo list - Will be here soon</p>}
+            body={<TasksList />}
        />
         
     </>
