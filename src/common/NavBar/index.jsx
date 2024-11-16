c2cefb40e6f376bfebfedfccfba8fd4acc4d3941
Jakub Musialowski <mds@mds.biz.pl>
2024-11-16 11:50:54 +0100

    Add navbar component

---
+++ src/common/NavBar/index.jsx
@@ -0,0 +1,7 @@
+const NavBar = () => (
+    <div className="p-3 bg-primaryGreyColor mb-8">
+        <p>Menu</p>
+    </div>
+);
+
+export default NavBar;
--- src/features/Root/root.jsx
+++ src/features/Root/root.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import MainElement from "../../common/Main";
+import NavBar from "../../common/NavBar";
 
 const RootElement = () =>  {
     return (
-        <div className="w-[min(1000px,calc(100%-20px))] mx-auto">
-            <MainElement>
-                <Outlet />
-            </MainElement>
-        </div>
+        <>
+            <NavBar />
+                <div className="w-[min(1000px,calc(100%-20px))] mx-auto">
+                <MainElement>
+                    <Outlet />
+                </MainElement>
+            </div>
+        </>
+        
     )
 };
 
