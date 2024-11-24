2417c1894da9d698afc6a2c0d7a033bf366ef099
Jakub Musialowski <mds@mds.biz.pl>
2024-11-24 20:53:56 +0100

    Add tasks list menu

--- src/common/PageSection/index.jsx
+++ src/common/PageSection/index.jsx
@@ -1,11 +1,12 @@
-const PageSection = ({title, body, menu}) => (
+const PageSection = ({title, body, headerMenu, menu}) => (
     <div className="border border-solid border-borderGray">
         {title ? (
             <div className="border-b border-solid border-b-borderGray p-3 bg-primaryGreyColor flex items-center">
                 <h2 className="text-lg/[1] font-bold grow">{title}</h2>
-                {menu}
+                {headerMenu}
             </div>
         ) : ""}
+        {menu}
         <div className="p-3">
             {body}
         </div>
---
+++ src/features/tasks/TasksMenu/index.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { taskSelector } from "../taskSlice";
+
+const TasksMenu = () => {
+    const tasks = useSelector(taskSelector);
+
+    return (
+        <div className="w-[calc(100%-20px)] mx-auto border-x border-b border-borderGray border-solid rounded-b">
+            <ul className="flex justify-end items-center gap-4 p-[5px] pr-2 text-sm">
+                <li>Zakończ wszystkie zadania</li>
+                <li>Ukryj zakończone</li>
+            </ul>
+        </div>
+    )
+};
+
+export default TasksMenu;
