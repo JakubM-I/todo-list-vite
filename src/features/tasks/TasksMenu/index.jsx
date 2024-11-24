889e75a1e59bdf2ab30d31b96fb7e50a036a9740
Jakub Musialowski <mds@mds.biz.pl>
2024-11-24 20:59:09 +0100

    Add condotion to display tasks menu

--- src/features/tasks/TasksMenu/index.jsx
+++ src/features/tasks/TasksMenu/index.jsx
@@ -3,14 +3,17 @@ import { taskSelector } from "../taskSlice";
 
 const TasksMenu = () => {
     const tasks = useSelector(taskSelector);
+    console.log(tasks.length);
 
     return (
-        <div className="w-[calc(100%-20px)] mx-auto border-x border-b border-borderGray border-solid rounded-b">
-            <ul className="flex justify-end items-center gap-4 p-[5px] pr-2 text-sm">
-                <li>Zakończ wszystkie zadania</li>
-                <li>Ukryj zakończone</li>
-            </ul>
-        </div>
+        <>
+            {tasks.length > 0 ? (<div className="w-[calc(100%-20px)] mx-auto border-x border-b border-borderGray border-solid rounded-b">
+                <ul className="flex justify-end items-center gap-4 p-[5px] pr-2 text-sm">
+                    <li>Zakończ wszystkie zadania</li>
+                    <li>Ukryj zakończone</li>
+                </ul>
+            </div>) : ""}
+        </>
     )
 };
 
