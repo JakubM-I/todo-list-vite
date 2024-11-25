def065d1f6c2552dc71f6e425e8d775d5c2005b6
Jakub Musialowski <mds@mds.biz.pl>
2024-11-25 21:10:44 +0100

    Change add button to reusable component

---
+++ src/common/AddButton/index.jsx
@@ -0,0 +1,9 @@
+const AddButton = ({title, onClick}) => (
+    <button className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue"
+    onClick={onClick}
+    >
+        {title}
+    </button>
+);
+
+export default AddButton;
--- src/features/tasks/AddMenu/index.jsx
+++ src/features/tasks/AddMenu/index.jsx
@@ -1,16 +1,21 @@
 import { useDispatch } from "react-redux";
 import { openModal } from "../../../common/PopupModal/modalSlice";
+import AddButton from "../../../common/AddButton";
 
 const AddMenu = () => {
     const dispatch = useDispatch();
 
     return (
         <div>
-            <button className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue"
+            <AddButton 
+                title="Dodaj zadanie"
+                onClick={() => dispatch(openModal())}
+            />
+            {/* <button className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue"
             onClick={() => dispatch(openModal())}
             >
                 Dodaj zadanie
-            </button>
+            </button> */}
         </div>
     )
 }
