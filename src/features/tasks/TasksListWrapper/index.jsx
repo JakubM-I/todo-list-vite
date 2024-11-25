e3a01f07608340376c01801a362c0d6838780383
Jakub Musialowski <mds@mds.biz.pl>
2024-11-25 21:08:13 +0100

    Add conditional rendering task list or empty tasks component

--- src/features/tasks/TasksListWrapper/index.jsx
+++ src/features/tasks/TasksListWrapper/index.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import TasksList from "../TaskList"
+import { useSelector } from "react-redux";
+import { taskSelector } from "../taskSlice";
+import EmptyTasksList from "../EmptyTasksList";
 
 const TasksListWrapper = () => {
+    const tasks = useSelector(taskSelector);
     return (
         <div className="flex gap-3">
-            <TasksList />
-            <Outlet />
+            { tasks.length > 0 ? (
+                <>
+                    <TasksList />
+                    <Outlet />
+                </>
+            ) : (<EmptyTasksList />)}
         </div>
     )
 };
