183481c9daa0c909129e1c32c4584ee948ccc3e5
Jakub Musialowski <mds@mds.biz.pl>
2024-11-26 21:18:44 +0100

    Add styling to hide done tasks form list

--- src/features/tasks/TaskList/index.jsx
+++ src/features/tasks/TaskList/index.jsx
@@ -35,62 +35,37 @@ const TasksList = () => {
                 [...tasks]
                 .sort((a, b) => a.taskDone - b.taskDone || b.taskPriority - a.taskPriority)
                 .map(task => (
-                    <li key={task.id} className="flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid">
-                    <button className="group shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-doneButton flex justify-center items-center text-[#fff] text-[20px]"
-                    onClick={() => dispatch(toggleTaskDone(task.id))}
-                    >
-                        {!task.taskDone ? (<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-[450ms] delay-100"><BiCheckCircle /></span>) : "" }
-                        
-                        {task.taskDone ? (<BiCheckCircle />) : ""}
-                    </button>
-                        <div className="grow">
-                            <div className={`cursor-pointer  ${task.taskDone ? "line-through text-disabledGray" : ""}`} 
-                            onClick={() => onTaskClick(task.id)}>
-                                {task.taskName}
-                            </div>
-                            {/* <NavLink to={`details/${task.id}`} >
-                                {task.taskName}
-                            </NavLink> */}
-                        </div>
-                    <div className="flex gap-[5px]">
-                        <span className="size-buttons flex justify-center items-center">
-                            {toggleTaskPriority(task.taskPriority)}
-                            {/* <img src={priority1} alt="" /> */}
-                        </span>
-                        <button 
-                            className="shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-removeButton flex justify-center items-center text-[#fff] text-[20px]"
-                            onClick={() => dispatch(removeTask(task.id))}
+                    <li key={task.id} className={`flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid ${task.taskVisibility ? "" : "hidden"}`}>
+                        <button className="group shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-doneButton flex justify-center items-center text-[#fff] text-[20px]"
+                        onClick={() => dispatch(toggleTaskDone(task.id))}
                         >
-                            <BiTrash />
+                            {!task.taskDone ? (<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-[450ms] delay-100"><BiCheckCircle /></span>) : "" }
+                            
+                            {task.taskDone ? (<BiCheckCircle />) : ""}
                         </button>
-                    </div>
-                </li>
+                            <div className="grow">
+                                <div className={`cursor-pointer  ${task.taskDone ? "line-through text-disabledGray" : ""}`} 
+                                onClick={() => onTaskClick(task.id)}>
+                                    {task.taskName}
+                                </div>
+                                {/* <NavLink to={`details/${task.id}`} >
+                                    {task.taskName}
+                                </NavLink> */}
+                            </div>
+                        <div className="flex gap-[5px]">
+                            <span className="size-buttons flex justify-center items-center">
+                                {toggleTaskPriority(task.taskPriority)}
+                                {/* <img src={priority1} alt="" /> */}
+                            </span>
+                            <button 
+                                className="shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-removeButton flex justify-center items-center text-[#fff] text-[20px]"
+                                onClick={() => dispatch(removeTask(task.id))}
+                            >
+                                <BiTrash />
+                            </button>
+                        </div>
+                    </li>
                 ))}
-            {/* <li className="flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid">
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-doneButton flex justify-center items-center text-[#fff] text-[20px]">
-                    <BiCheckCircle />
-                </button>
-                    <div className="grow">
-                    Zadanie 1
-                    </div>
-                <div className="flex gap-[5px]">
-                    <span className="size-buttons flex justify-center items-center"><img src={priority1} alt="" /></span>
-                    <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-removeButton flex justify-center items-center text-[20px]">
-                        <BiTrash />
-                    </button>
-                </div>
-            </li>
-            <li className="flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid">
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-doneButton flex justify-center items-center  text-[#fff] text-[20px]">
-                    <BiCheckCircle />
-                </button>
-                    <div className="grow">
-                    Zadanie 2
-                    </div>
-                <button className="shrink-0 border border-solid border-borderGray w-buttons h-buttons bg-removeButton flex justify-center items-center text-[20px]">
-                    <BiTrash />
-                </button>
-            </li> */}
         </ul>
     )
 };
