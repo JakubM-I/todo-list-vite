68f5869b46a7b75ffb86ccdeeebbf0bd330ad4ca
Jakub Musialowski <mds@mds.biz.pl>
2024-11-26 21:19:32 +0100

    Add toogle button text on hide done task button

--- src/features/tasks/TasksMenu/index.jsx
+++ src/features/tasks/TasksMenu/index.jsx
@@ -1,16 +1,27 @@
-import { useSelector } from "react-redux";
-import { taskSelector } from "../taskSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { anyHiddenTask, hideDoneTasks, taskSelector, toggleAllTaskDone } from "../taskSlice";
 
 const TasksMenu = () => {
     const tasks = useSelector(taskSelector);
-    console.log(tasks.length);
+    const isHiiden = useSelector(anyHiddenTask);
+    console.log(isHiiden)
+    const dispatch = useDispatch();
+
 
     return (
         <>
             {tasks.length > 0 ? (<div className="w-[calc(100%-20px)] mx-auto border-x border-b border-borderGray border-solid rounded-b">
                 <ul className="flex justify-end items-center gap-4 p-[5px] pr-2 text-sm">
-                    <li>Zakończ wszystkie zadania</li>
-                    <li>Ukryj zakończone</li>
+                    <li>
+                        <button onClick={() => dispatch(toggleAllTaskDone())}>
+                            Zakończ wszystkie zadania
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => dispatch(hideDoneTasks())}>
+                            {isHiiden ? "Pokaż" : "Ukryj "} zakończone
+                        </button>
+                    </li>
                 </ul>
             </div>) : ""}
         </>
