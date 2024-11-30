2fdff115bf5ca63371840b74146e8deb2d4c102b
Jakub Musialowski <mds@mds.biz.pl>
2024-11-30 20:49:43 +0100

    Change link to button closed task page

--- src/features/tasks/TaskPage/index.jsx
+++ src/features/tasks/TaskPage/index.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { searchTaskById } from "../taskSlice";
 import priority1 from "../../../assets/priority-1-icon.png";
 import priority2 from "../../../assets/priority-2-icon.png"
@@ -7,6 +7,7 @@ import { BiCheckCircle } from "react-icons/bi";
 
 const TaskPage = () => {
     const {id} = useParams();
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const query = searchParams.get("szukaj");
     const task = useSelector(state => searchTaskById(state, id));
@@ -21,15 +22,19 @@ const TaskPage = () => {
         }
 
         return <img src={priority1} alt="" className="w-[1.2em] h-[1.2em]"/> 
+    };
+
+    const onClickBack = () => {
+        navigate(`/${query ? `?szukaj=${query}` : ""}`)
     }
 
     return (
         <div className="w-[50%]">  
             <div>            
                 <span className="flex justify-end">
-                    <Link to={`/${query ? `?szukaj=${query}` : ""}`} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
+                    <button onClick={onClickBack} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="currentColor" d="M5.146 5.146a.5.5 0 0 1 .708 0L12 11.293l6.146-6.147a.5.5 0 0 1 .638-.057l.07.057a.5.5 0 0 1 0 .708L12.707 12l6.147 6.146a.5.5 0 0 1 .057.638l-.057.07a.5.5 0 0 1-.708 0L12 12.707l-6.146 6.147a.5.5 0 0 1-.638.057l-.07-.057a.5.5 0 0 1 0-.708L11.293 12 5.146 5.854a.5.5 0 0 1-.057-.638z"></path></svg>
-                    </Link>
+                    </button>
                 </span>
             </div>
             <div className="flex justify-start items-center border-t border-x rounded-t border-solid border-borderGray bg-primaryGreyColor">
