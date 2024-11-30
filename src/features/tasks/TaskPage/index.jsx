37197fb68694d735be0f7d7e7316d8c5ea6d3815
Jakub Musialowski <mds@mds.biz.pl>
2024-11-30 20:15:42 +0100

    Corrected back link to tasks from task page

--- src/features/tasks/TaskPage/index.jsx
+++ src/features/tasks/TaskPage/index.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 import { searchTaskById } from "../taskSlice";
 import priority1 from "../../../assets/priority-1-icon.png";
 import priority2 from "../../../assets/priority-2-icon.png"
@@ -7,6 +7,8 @@ import { BiCheckCircle } from "react-icons/bi";
 
 const TaskPage = () => {
     const {id} = useParams();
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get("szukaj");
     const task = useSelector(state => searchTaskById(state, id));
 
     const toggleTaskPriority = (taskPriority) => {
@@ -25,7 +27,7 @@ const TaskPage = () => {
         <div className="w-[50%]">  
             <div>            
                 <span className="flex justify-end">
-                    <Link to={"/"} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
+                    <Link to={`/${query ? `?szukaj=${query}` : ""}`} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="currentColor" d="M5.146 5.146a.5.5 0 0 1 .708 0L12 11.293l6.146-6.147a.5.5 0 0 1 .638-.057l.07.057a.5.5 0 0 1 0 .708L12.707 12l6.147 6.146a.5.5 0 0 1 .057.638l-.057.07a.5.5 0 0 1-.708 0L12 12.707l-6.146 6.147a.5.5 0 0 1-.638.057l-.07-.057a.5.5 0 0 1 0-.708L11.293 12 5.146 5.854a.5.5 0 0 1-.057-.638z"></path></svg>
                     </Link>
                 </span>
