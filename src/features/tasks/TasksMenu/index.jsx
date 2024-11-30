7d5bb10e9d04d7e03b06f2f196092b88feaf3fa0
Jakub Musialowski <mds@mds.biz.pl>
2024-11-30 10:58:03 +0100

    Change task menu ittem order and add conditional rendering

--- src/features/tasks/TasksMenu/index.jsx
+++ src/features/tasks/TasksMenu/index.jsx
@@ -1,29 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
-import { allTaskDone, anyHiddenTask, hideDoneTasks, taskSelector, toggleAllTaskDone } from "../taskSlice";
+import { allTaskDone, anyHiddenTask, hideDoneTasks, AnyTaskDone, taskSelector, toggleAllTaskDone } from "../taskSlice";
 
 const TasksMenu = () => {
     const tasks = useSelector(taskSelector);
     const isHiiden = useSelector(anyHiddenTask);
     const isAllTaskDone = useSelector(allTaskDone);
     console.log(isHiiden)
+    const isAnyTaskDone = useSelector(AnyTaskDone);
     const dispatch = useDispatch();
 
     return (
         <>
             {tasks.length > 0 && (<div className="w-[calc(100%-20px)] mx-auto border-x border-b border-borderGray border-solid rounded-b">
                 <ul className="flex flex-wrap justify-center items-center gap-2 p-[5px] pr-2 text-sm mobile-l:justify-end mobile-l:flex-nowrap mobile-l:gap-4">
+                    {isAnyTaskDone && (
                     <li>
                         <button 
-                            className="disabled:text-disabledGray"
+                            className="hover:text-primaryBlueHover"
+                            onClick={() => dispatch(hideDoneTasks())}>
+                            {isHiiden ? "Pokaż" : "Ukryj "} zakończone
+                        </button>
+                    </li>)}
+                    <li>
+                        <button 
+                            className="disabled:text-disabledGray text-primaryBlue hover:text-primaryBlueHover"
                             onClick={() => dispatch(toggleAllTaskDone())}
                             disabled={isAllTaskDone ? true : false}
                         >
-                            Zakończ wszystkie zadania
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => dispatch(hideDoneTasks())}>
-                            {isHiiden ? "Pokaż" : "Ukryj "} zakończone
+                            Zakończ wszystkie
                         </button>
                     </li>
                 </ul>
