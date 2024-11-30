f1901a7b924224045a6c1e0e5ec2f284512dc3cc
Jakub Musialowski <mds@mds.biz.pl>
2024-11-30 11:28:50 +0100

    Pass query to url from search input

--- src/common/SearchBar/index.jsx
+++ src/common/SearchBar/index.jsx
@@ -1,14 +1,39 @@
+import { useRef, useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import { useSearchParams } from "react-router-dom";
 
-const SearchBar = () => (
-    <form action="" className="flex justify-end items-center gap-2">
-        <input className="p-[4px] text-[14px]/[1] text-primaryTextColor placeholder:text-[14px]/[1]" 
-            type="text"
-            placeholder="Wyszukaj zadanie"  />
-        <button className="appearance-none p-1 bg-borderGray border rounded-lg border-borderGray transition easy-in duration-700 hover:bg-secondaryText">
-            <BiSearch />
-        </button>
-    </form>
-);
+const SearchBar = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState(searchParams.get("szukaj"));
+    const inputRef = useRef(null);
+
+    const searchOnSubmit = (e) => {
+        e.preventDefault();
+        setSearchParams(query.trim() 
+            ? {szukaj: query} 
+            : searchParams.delete("szukaj"))
+        setQuery("");
+        inputRef.current.focus();
+    }
+
+    return (
+        <form action="" className="flex justify-end items-center gap-2" onSubmit={searchOnSubmit}>
+            <input 
+                className="p-[4px] text-[14px]/[1] text-primaryTextColor placeholder:text-[14px]/[1]" 
+                type="text"
+                ref={inputRef}
+                value={query ? query : ""}
+                placeholder="Wyszukaj zadanie" 
+                onChange={({target}) => setQuery(target.value)}
+             />
+            <button 
+                className="appearance-none p-1 bg-borderGray border rounded-lg border-borderGray transition easy-in duration-700 hover:bg-secondaryText"
+                
+            >
+                <BiSearch />
+            </button>
+        </form>
+    )
+};
 
 export default SearchBar;
