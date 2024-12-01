5880dd29f56d7a484f910bc15d39d3890583a440
Jakub Musialowski <mds@mds.biz.pl>
2024-12-01 01:11:19 +0100

    Add sorting tasks by date

--- src/features/tasks/TaskList/index.jsx
+++ src/features/tasks/TaskList/index.jsx
@@ -5,6 +5,7 @@ import priority2 from "../../../assets/priority-2-icon.png"
 import { useDispatch, useSelector } from "react-redux";
 import { openAddForm, removeTask, searchTaskBtQuery, taskSelector, toggleTaskDone } from "../taskSlice";
 import { NavLink, useNavigate, useSearchParams } from "react-router-dom";
+import { nanoid } from "@reduxjs/toolkit";
 
 
 const TasksList = () => {
@@ -31,43 +32,67 @@ const TasksList = () => {
         // dispatch(openAddForm())
     }
 
+    const allDatesList = tasks.map(task => ({ id: nanoid(), date: task.taskDate }))
+
+    const datesList = [...new Map(allDatesList.map((m) => [m.date || "no-date", m])).values()];
+
     return (
         <ul className="grow">
-            {
-                [...tasks]
-                .sort((a, b) => a.taskDone - b.taskDone || b.taskPriority - a.taskPriority)
-                .map(task => (
-                    <li key={task.id} className={`flex justify-between items-center gap-3 p-[5px] w-full border-b border-b-borderGray border-b-solid ${task.taskVisibility ? "" : "hidden"}`}>
-                        <button className="group shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-doneButton flex justify-center items-center text-[#fff] text-[20px]"
-                        onClick={() => dispatch(toggleTaskDone(task.id))}
-                        >
-                            {!task.taskDone ? (<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-[450ms] delay-100"><BiCheckCircle /></span>) : "" }
-                            
-                            {task.taskDone ? (<BiCheckCircle />) : ""}
-                        </button>
-                            <div className="grow">
-                                <div className={`cursor-pointer  ${task.taskDone ? "line-through text-disabledGray" : ""}`} 
-                                onClick={() => onTaskClick(task.id)}>
-                                    {task.taskName}
-                                </div>
-                                {/* <NavLink to={`details/${task.id}`} >
-                                    {task.taskName}
-                                </NavLink> */}
-                            </div>
-                        <div className="flex gap-[5px]">
-                            <span className="size-buttons flex justify-center items-center">
-                                {toggleTaskPriority(task.taskPriority)}
-                                {/* <img src={priority1} alt="" /> */}
-                            </span>
-                            <button 
-                                className="shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-removeButton flex justify-center items-center text-[#fff] text-[20px]"
-                                onClick={() => dispatch(removeTask(task.id))}
-                            >
-                                <BiTrash />
-                            </button>
-                        </div>
+            {[...datesList]
+                .sort((a, b) => {
+                    if(!a.date) return -1;
+                    if(!b.date) return 1;
+
+                    return new Date(b.date) - new Date(a.date);
+                })
+                .map(date => (
+                    <li 
+                        key={date.id}
+                        className="pb-2"
+                    >
+                        {date.date && (
+                            <h3 className="pb-2 font-bold text-lg">
+                                {date.date}
+                            </h3>
+                        )}
+                            {[...tasks]
+                            .filter(task => task.taskDate === date.date)
+                            .sort((a, b) => a.taskDone - b.taskDone || b.taskPriority - a.taskPriority)
+                            .map(task => (
+                                <li key={task.id} className={`flex justify-between items-center gap-3 px-[5px] py-2 w-full border-b border-b-borderGray border-b-solid ${task.taskVisibility ? "" : "hidden"}`}>
+                                    <button className="group shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-doneButton flex justify-center items-center text-[#fff] text-[20px]"
+                                    onClick={() => dispatch(toggleTaskDone(task.id))}
+                                    >
+                                        {!task.taskDone ? (<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-[450ms] delay-100"><BiCheckCircle /></span>) : "" }
+                                        
+                                        {task.taskDone ? (<BiCheckCircle />) : ""}
+                                    </button>
+                                        <div className="grow">
+                                            <div className={`cursor-pointer text-base ${task.taskDone ? "line-through text-disabledGray" : ""}`} 
+                                            onClick={() => onTaskClick(task.id)}>
+                                                {task.taskName}
+                                            </div>
+                                            {/* <NavLink to={`details/${task.id}`} >
+                                                {task.taskName}
+                                            </NavLink> */}
+                                        </div>
+                                    <div className="flex gap-[5px] items-center">
+                                        <span className="size-buttons flex justify-center items-center">
+                                            {toggleTaskPriority(task.taskPriority)}
+                                            {/* <img src={priority1} alt="" /> */}
+                                        </span>
+                                        <button 
+                                            className="shrink-0 border border-solid border-borderGray rounded-full w-buttons h-buttons bg-removeButton flex justify-center items-center text-[#fff] text-[20px]"
+                                            onClick={() => dispatch(removeTask(task.id))}
+                                        >
+                                            <BiTrash />
+                                        </button>
+                                    </div>
+                                </li>
+                            ))}
                     </li>
-                ))}
+                ))
+            }
         </ul>
         
     )
