9ce562bfd33c6b987e2cd43b375323ea8d634717
Jakub Musialowski <mds@mds.biz.pl>
2024-12-01 22:02:24 +0100

    Main categorys page structure

--- src/features/categories/CategoryList/index.jsx
+++ src/features/categories/CategoryList/index.jsx
@@ -1,6 +1,19 @@
 const CategoryList = () => {
     return (
-        <p>Tu będzie lista categorii</p>
+        <ul className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2">
+            <li className="border border-borderGray p-2">
+                <h2 className="text-primaryTextColor text-base">Osobiste</h2>
+            </li>
+            <li className="border border-borderGray p-2">
+                <h2 className="text-primaryTextColor text-base">Praca</h2>
+            </li>
+            <li className="border border-borderGray p-2">
+                <h2 className="text-primaryTextColor text-base">Domowe</h2>
+            </li>
+            <li className="border border-borderGray p-2">
+                <h2 className="text-primaryTextColor text-base">Wakacje</h2>
+            </li>
+        </ul>
     )
 };
 
