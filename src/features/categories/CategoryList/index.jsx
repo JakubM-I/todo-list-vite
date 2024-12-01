19b2f70945d9a8ccb828942ec323392ef1fe3988
Jakub Musialowski <mds@mds.biz.pl>
2024-12-01 22:46:53 +0100

    Category list item buttons styling

--- src/features/categories/CategoryList/index.jsx
+++ src/features/categories/CategoryList/index.jsx
@@ -10,29 +10,69 @@ const CategoryList = () => {
             <li className="border border-borderGray p-2 flex justify-start items-center gap-2">
                 <h2 className="text-primaryTextColor text-base">Osobiste</h2>
                 <div className="flex justify-center items-center gap-2 ml-auto mr-0">
-                    <button><BiEditAlt /></button>
-                    <button><BiTrash /></button>
+                    <button 
+                        title="Edytuj"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiEditAlt />
+                    </button>
+                    <button
+                        title="Usuń"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiTrash />
+                    </button>
                 </div>
             </li>
             <li className="border border-borderGray p-2 flex justify-start items-center gap-2">
                 <h2 className="text-primaryTextColor text-base">Praca</h2>
                 <div className="flex justify-center items-center gap-2 ml-auto mr-0">
-                    <button><BiEditAlt /></button>
-                    <button><BiTrash /></button>
+                    <button
+                        title="Edytuj"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiEditAlt />
+                    </button>
+                    <button
+                        title="Usuń"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiTrash />
+                    </button>
                 </div>
             </li>
             <li className="border border-borderGray p-2 flex justify-start items-center gap-2">
                 <h2 className="text-primaryTextColor text-base">Domowe</h2>
                 <div className="flex justify-center items-center gap-2 ml-auto mr-0">
-                    <button><BiEditAlt /></button>
-                    <button><BiTrash /></button>
+                    <button
+                        title="Edytuj"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiEditAlt />
+                    </button>
+                    <button
+                        title="Usuń"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiTrash />
+                    </button>
                 </div>
             </li>
             <li className="border border-borderGray p-2 flex justify-start items-center gap-2">
                 <h2 className="text-primaryTextColor text-base">Wakacje</h2>
                 <div className="flex justify-center items-center gap-2 ml-auto mr-0">
-                    <button><BiEditAlt /></button>
-                    <button><BiTrash /></button>
+                    <button
+                        title="Edytuj"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiEditAlt />
+                    </button>
+                    <button
+                        title="Usuń"
+                        className="p-[3px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                    >
+                        <BiTrash />
+                    </button>
                 </div>
             </li>
         </ul>
