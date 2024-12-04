307c6d48bf0a21f85cc4c1953e56446384b0d278
Jakub Musialowski <mds@mds.biz.pl>
2024-12-04 21:48:37 +0100

    Corrected local storage function for cateogrie

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -5,9 +5,26 @@ export const saveTaskToLocalStorage = (tasks) => {
 export const loadTaskFromLocalStorage = () => 
     JSON.parse(localStorage.getItem("tasks")) || [];
 
-export const saveCategoryToLocalStorage = (tasks) => {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+export const saveCategoryToLocalStorage = (categories) => {
+    localStorage.setItem("categories", JSON.stringify(categories))
 };
 
 export const loadCategoryFromLocalStorage = () => 
-    JSON.parse(localStorage.getItem("tasks")) || [];
+    JSON.parse(localStorage.getItem("categories")) || [
+        {
+            "categoryId": "2",   
+            "categoryName": "Osobiste"
+        },
+        {
+            "categoryId": "2",   
+            "categoryName": "Praca"
+        },
+        {
+            "categoryId": "3",   
+            "categoryName": "Domowe"
+        },
+        {
+            "categoryId": "4",   
+            "categoryName": "Sport"
+        }
+    ];
