fe214b378b0777ae253cfc363d0d4a446ac742c4
Jakub Musialowski <mds@mds.biz.pl>
2024-12-05 22:56:36 +0100

    Add onclick function to add category button

--- src/features/categories/ButtonMenu/index.jsx
+++ src/features/categories/ButtonMenu/index.jsx
@@ -1,9 +1,17 @@
+import { useDispatch } from "react-redux";
 import AddButton from "../../../common/AddButton"
+import { openModal } from "../../../common/PopupModal/modalSlice";
 
 const ButtonMenu = () => {
+    const dispatch = useDispatch();
+    const openElement = {
+        title: "Nowe zadanie",
+        body: "categoryForm",
+    }
     return (
         <AddButton 
             title="Dodaj kategorię"
+            onClick={() => dispatch(openModal(openElement))}
         />
     )
 };
