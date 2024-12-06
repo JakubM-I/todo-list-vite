18f3bc7664aa330620c2197c1d70aafce684c978
Jakub Musialowski <mds@mds.biz.pl>
2024-12-06 23:41:31 +0100

    Change conditional rendering and back function call on task page

--- src/features/tasks/TaskPage/index.jsx
+++ src/features/tasks/TaskPage/index.jsx
@@ -32,7 +32,7 @@ const TaskPage = () => {
         <div className="w-[50%]">  
             <div>            
                 <span className="flex justify-end">
-                    <button onClick={onClickBack} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
+                    <button onClick={() => onClickBack()} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="currentColor" d="M5.146 5.146a.5.5 0 0 1 .708 0L12 11.293l6.146-6.147a.5.5 0 0 1 .638-.057l.07.057a.5.5 0 0 1 0 .708L12.707 12l6.147 6.146a.5.5 0 0 1 .057.638l-.057.07a.5.5 0 0 1-.708 0L12 12.707l-6.146 6.147a.5.5 0 0 1-.638.057l-.07-.057a.5.5 0 0 1 0-.708L11.293 12 5.146 5.854a.5.5 0 0 1-.057-.638z"></path></svg>
                     </button>
                 </span>
@@ -44,8 +44,8 @@ const TaskPage = () => {
              <div className="border rounded-b border-solid border-borderGray p-2">
                 <p className="block text-sm/[1.2] mb-3">{task.taskDesc}</p>
                 <div className="flex gap-2 items-center">
-                    {task.taskDate ? (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskDate}</p>) : ""}
-                    {task.taskCategory ? (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskCategory}</p>) : ""}
+                    {task.taskDate && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskDate}</p>)}
+                    {task.taskCategory && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskCategory}</p>)}
                     {task.taskPriority === "0" || task.taskPriority === "1" ? "" : (<p className="block text-xs/[1] p-[4px] border border-solid border-borderGray rounded">{toggleTaskPriority(task.taskPriority)}</p>) }
                 </div>
              </div>
