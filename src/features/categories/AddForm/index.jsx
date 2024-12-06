5c5de3e73d09e77d1122030bf3767d533b1884b3
Jakub Musialowski <mds@mds.biz.pl>
2024-12-06 23:25:47 +0100

    Category add form added

--- src/features/categories/AddForm/index.jsx
+++ src/features/categories/AddForm/index.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addCategory } from "../categorySlice";
+import { nanoid } from "@reduxjs/toolkit";
+
 const AddCategoryForm = () => {
+    const dispatch = useDispatch();
+    const [categoryName, setCategoryName] = useState();
+
+    const categorySubmit = (e) => {
+        e.preventDefault();
+        if(!categoryName.trim()){
+            return;
+        }
+
+        dispatch(addCategory({
+            categoryId: nanoid(),
+            categoryName: categoryName.trim(),
+        }))
+    }
+
     return (
-        <p>Dodaj kategorie</p>
+        <form 
+            onSubmit={categorySubmit}
+            className="flex flex-col gap-[10px]"
+        >
+            <input 
+                className="border-b border-solid border-borderGray p-1 text-sm/[1.2] placeholder:text-secondaryText placeholder:text-sm" 
+                type="text" 
+                value={categoryName}
+                onChange={({target}) => setCategoryName(target.value)}
+            />
+            <button>Zapisz</button>
+        </form>
     )
 };
 
