25abedae32782e40986fc4b9f5fb830c723a7fb6
Jakub Musialowski <mds@mds.biz.pl>
2024-12-07 22:59:26 +0100

    Add new reducer - edit category

--- src/features/categories/categorySlice.jsx
+++ src/features/categories/categorySlice.jsx
@@ -14,6 +14,11 @@ const categorySlice = createSlice({
         deleteCategory: ({categories}, {payload: categoryId}) => {
             const categoryIndex = categories.findIndex(category => category.categoryId === categoryId);
             categories.splice(categoryIndex, 1);
+        },
+
+        editCategory: ({categories}, {payload}) => {
+            const index = categories.findIndex(category => category.categoryId === payload.categoryId);
+            categories[index] = {...categories[index], categoryName: payload.categoryName}
         }
     },
 
@@ -23,5 +28,5 @@ const categorySlice = createSlice({
 export const categoryStateSelector = state => state.categories;
 export const categorySelector = state => categoryStateSelector(state).categories;
 
-export const {addCategory, deleteCategory} = categorySlice.actions;
+export const {addCategory, deleteCategory, editCategory} = categorySlice.actions;
 export default categorySlice.reducer;
