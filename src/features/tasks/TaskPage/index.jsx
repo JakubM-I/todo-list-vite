547d16efe790c66927a54025650591fff763829b
Jakub Musialowski <mds@mds.biz.pl>
2024-12-08 22:04:44 +0100

    Add edut button to task page

--- src/features/tasks/TaskPage/index.jsx
+++ src/features/tasks/TaskPage/index.jsx
@@ -4,11 +4,14 @@ import { searchTaskById } from "../taskSlice";
 import priority1 from "../../../assets/priority-1-icon.png";
 import priority2 from "../../../assets/priority-2-icon.png"
 import { BiCheckCircle } from "react-icons/bi";
+import { BiEditAlt } from "react-icons/bi";
+import { useState } from "react";
 
 const TaskPage = () => {
     const {id} = useParams();
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
+    const [isEdit, setIsEdit] = useState(false);
     const query = searchParams.get("szukaj");
     const task = useSelector(state => searchTaskById(state, id));
 
@@ -37,19 +40,28 @@ const TaskPage = () => {
                     </button>
                 </span>
             </div>
-            <div className="flex justify-start items-center border-t border-x rounded-t border-solid border-borderGray bg-primaryGreyColor">
-            <span className="flex justify-center items-center pl-2 text-doneButton">{task.taskDone ? (<BiCheckCircle />) : ""}</span>
-            <h3 className="  font-bold text-base/[1] p-2 ">{task.taskName}</h3>
-            </div>
-             <div className="border rounded-b border-solid border-borderGray p-2">
-                <p className="block text-sm/[1.2] mb-3">{task.taskDesc}</p>
-                <div className="flex gap-2 items-center">
-                    {task.taskDate && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskDate}</p>)}
-                    {task.taskCategory && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskCategory}</p>)}
-                    {task.taskPriority === "0" || task.taskPriority === "1" ? "" : (<p className="block text-xs/[1] p-[4px] border border-solid border-borderGray rounded">{toggleTaskPriority(task.taskPriority)}</p>) }
-                </div>
-             </div>
-
+            {isEdit ? (<p>Edycja zadania</p>) : (
+                <>
+                    <div className="flex justify-start items-center border-t border-x rounded-t border-solid border-borderGray bg-primaryGreyColor">
+                        <span className="flex justify-center items-center pl-2 text-doneButton">{task.taskDone ? (<BiCheckCircle />) : ""}</span>
+                        <h3 className="font-bold text-base/[1] p-2 ">{task.taskName}</h3>
+                        <button 
+                            className="ml-auto mr-2 p-[3px] border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded"
+                            onClick={() => setIsEdit(true)}
+                        >
+                            <BiEditAlt />
+                        </button>
+                    </div>
+                    <div className="border rounded-b border-solid border-borderGray p-2">
+                        <p className="block text-sm/[1.2] mb-3">{task.taskDesc}</p>
+                        <div className="flex gap-2 items-center">
+                            {task.taskDate && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskDate}</p>)}
+                            {task.taskCategory && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskCategory}</p>)}
+                            {task.taskPriority === "0" || task.taskPriority === "1" ? "" : (<p className="block text-xs/[1] p-[4px] border border-solid border-borderGray rounded">{toggleTaskPriority(task.taskPriority)}</p>) }
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 };
