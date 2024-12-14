f1d22a81c8db98e7474037962077990221e1a40b
Jakub Musialowski <mds@mds.biz.pl>
2024-12-14 21:58:59 +0100

    Add task category and moved priority on tasks list

--- src/features/tasks/TaskList/index.jsx
+++ src/features/tasks/TaskList/index.jsx
@@ -63,36 +63,45 @@ const TasksList = () => {
                             .filter(task => task.taskDate === date.date)
                             .sort((a, b) => a.taskDone - b.taskDone || b.taskPriority - a.taskPriority)
                             .map(task => (
-                                <li key={task.id} className={`flex justify-between items-center gap-3 px-[5px] py-2 w-full border-b border-b-borderGray border-b-solid ${task.taskVisibility ? "" : "hidden"}`}>
-                                    <button className="group shrink-0 border border-solid border-doneButton rounded-full w-[22px] h-[22px] flex justify-center items-center text-doneButton text-[20px]"
-                                    onClick={() => dispatch(toggleTaskDone(task.id))}
-                                    >
-                                        {!task.taskDone ? (<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-[450ms] delay-100"><BiCheck /></span>) : "" }
-                                        
-                                        {task.taskDone ? (<BiCheck />) : ""}
-                                    </button>
-                                        <div className="grow">
-                                            <div className={`cursor-pointer text-base ${task.taskDone ? "line-through text-disabledGray" : ""}`} 
-                                            onClick={() => onTaskClick(task.id)}>
-                                                {task.taskName}
-                                            </div>
-                                            {/* <NavLink to={`details/${task.id}`} >
-                                                {task.taskName}
-                                            </NavLink> */}
-                                        </div>
-                                    <div className="flex gap-[5px] items-center">
-                                        <span className="size-buttons flex justify-center items-center">
-                                            {toggleTaskPriority(task.taskPriority)}
-                                            {/* <img src={priority1} alt="" /> */}
-                                        </span>
-                                        <button 
-                                            title="Usuń"
-                                            className="shrink-0 p-[3px] border border-solid border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded w-[24px] h-[24px] flex justify-center items-center text-primaryTextColor text-[18px]"
-                                            onClick={() => dispatch(removeTask(task.id))}
+                                <li key={task.id} className={`px-[5px] py-2 w-full border-b border-b-borderGray border-b-solid ${task.taskVisibility ? "" : "hidden"}`}>
+                                    <div className="flex justify-between items-center gap-3 mb-2">
+                                        <button className="group shrink-0 border border-solid border-doneButton rounded-full w-[22px] h-[22px] flex justify-center items-center text-doneButton text-[20px]"
+                                        onClick={() => dispatch(toggleTaskDone(task.id))}
                                         >
-                                            <BiTrash />
+                                            {!task.taskDone ? (<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-[450ms] delay-100"><BiCheck /></span>) : "" }
+                                            
+                                            {task.taskDone ? (<BiCheck />) : ""}
                                         </button>
+                                            <div className="grow">
+                                                <div className={`cursor-pointer text-base ${task.taskDone ? "line-through text-disabledGray" : ""}`} 
+                                                onClick={() => onTaskClick(task.id)}>
+                                                    {task.taskName}
+                                                </div>
+                                                {/* <NavLink to={`details/${task.id}`} >
+                                                    {task.taskName}
+                                                </NavLink> */}
+                                            </div>
+                                        <div className="flex gap-[5px] items-center">
+                                            {/* <span className="size-buttons flex justify-center items-center">
+                                                {toggleTaskPriority(task.taskPriority)}
+                                        
+                                            </span> */}
+                                            <button 
+                                                title="Usuń"
+                                                className="shrink-0 p-[3px] border border-solid border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded w-[24px] h-[24px] flex justify-center items-center text-primaryTextColor text-[18px]"
+                                                onClick={() => dispatch(removeTask(task.id))}
+                                            >
+                                                <BiTrash />
+                                            </button>
+                                        </div>
                                     </div>
+                                    <div className="flex justify-start items-center gap-2">
+                                        <span className="w-[22px] h-[22px] flex justify-center items-center">
+                                            {toggleTaskPriority(task.taskPriority)}   
+                                        </span>
+                                        {task.taskCategory && (<p className="block text-xs/[1] p-[5px] border border-solid border-borderGray rounded">{task.taskCategory}</p>)}
+                                    </div>
+                                    
                                 </li>
                             ))}
                            </ul>
