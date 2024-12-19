48586ee9dc72832eeb80101781b1cc5ee6e5a94d
Jakub Musialowski <mds@mds.biz.pl>
2024-12-19 21:53:25 +0100

    Add styling to config form

--- src/features/configuration/ConfigurationPage/index.jsx
+++ src/features/configuration/ConfigurationPage/index.jsx
@@ -4,7 +4,6 @@ const ConfigurationPage = () => {
 
     return (
         <>
-        <p>Konfiguracja</p>
         <SortTypeSwitcher />
         </>
     )
--- src/features/tasks/SortTypeSwitcher/index.jsx
+++ src/features/tasks/SortTypeSwitcher/index.jsx
@@ -15,11 +15,12 @@ const SortTypeSwitcher = () => {
 
     return (
         <div>
-            <p>Domyślne sortowanie</p>
-            <form onSubmit={onFormSubmit}>
-               <div>
-                    <div>
-                        <label htmlFor="date">Data</label>
+            <p className="pb-2 font-bold text-base">Domyślne sortowanie</p>
+            <form onSubmit={onFormSubmit}
+                className="ml-2"
+            >
+               <div className="mb-2">
+                    <div className="flex justify-start items-center gap-2">
                         <input 
                             type="radio" 
                             name="sortType" 
@@ -27,11 +28,11 @@ const SortTypeSwitcher = () => {
                             value="date" 
                             checked={selectedSortType === "date"}
                             onChange={({target}) => setSelectedSortType(target.value)}
-                            
+                            className="cursor-pointer"
                         />
+                        <label className="cursor-pointer" htmlFor="date">Data</label>
                     </div>
-                    <div>
-                        <label htmlFor="category">Kategorie</label>
+                    <div className="flex justify-start items-center gap-2">
                         <input 
                             type="radio" 
                             name="sortType" 
@@ -39,10 +40,14 @@ const SortTypeSwitcher = () => {
                             value="category" 
                             checked={selectedSortType === "category"}
                             onChange={({target}) => setSelectedSortType(target.value)}
+                            className="cursor-pointer"
                         />
+                        <label className="cursor-pointer" htmlFor="category">Kategorie</label>
                     </div>
                 </div>
-                <button>Zpisz</button>
+                <button className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue">
+                    Zpisz
+                </button>
             </form>
         </div>
     )
