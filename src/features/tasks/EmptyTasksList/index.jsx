2352bff05c47c14f84a78ccba99cfb274de43280
Jakub Musialowski <mds@mds.biz.pl>
2024-12-20 23:52:48 +0100

    Change dispatching action in example task load button

--- src/features/tasks/EmptyTasksList/index.jsx
+++ src/features/tasks/EmptyTasksList/index.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { fetchEXampleTask } from "../taskSlice";
+import { fetchExampleData } from "../../configuration/configurationSlice";
 
 const EmptyTasksList = () => {
     const dispatch = useDispatch();
@@ -9,7 +10,7 @@ const EmptyTasksList = () => {
             <p className="text-center mb-2">Dodaj swoje pierwsze zadanie</p>
             <span className="flex justify-center items-center gap-1">
                 <p>lub</p>
-                <button onClick={() => dispatch(fetchEXampleTask())} 
+                <button onClick={() => dispatch(fetchExampleData())} 
                     className="text-exmapleTaskButton hover:text-exmapleTaskButtonHover "
                 >
                     wczytaj przykładowe zadania
