cd9a28e01419dafc844ae396ced6fe3d9edcf3a5
Jakub Musialowski <mds@mds.biz.pl>
2024-12-21 00:30:15 +0100

    Add conditional rednering information about loading data

--- src/features/tasks/EmptyTasksList/index.jsx
+++ src/features/tasks/EmptyTasksList/index.jsx
@@ -1,20 +1,28 @@
-import { useDispatch } from "react-redux";
-import { fetchExampleData } from "../../configuration/configurationSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { configLoadingState, fetchExampleData } from "../../configuration/configurationSlice";
 
 const EmptyTasksList = () => {
     const dispatch = useDispatch();
+    const isLoading = useSelector(configLoadingState);
     
     return (
         <div className="grow">
-            <p className="text-center mb-2">Dodaj swoje pierwsze zadanie</p>
-            <span className="flex justify-center items-center gap-1">
-                <p>lub</p>
-                <button onClick={() => dispatch(fetchExampleData())} 
-                    className="text-exmapleTaskButton hover:text-exmapleTaskButtonHover "
-                >
-                    wczytaj przykładowe zadania
-                </button>
-            </span>
+            {isLoading ? 
+                (<p className="text-center mb-2">Ładowanie danych...</p>) : 
+                (
+                <>
+                    <p className="text-center mb-2">Dodaj swoje pierwsze zadanie</p>
+                    <span className="flex justify-center items-center gap-1">
+                    <p>lub</p>
+                    <button onClick={() => dispatch(fetchExampleData())} 
+                        className="text-exmapleTaskButton hover:text-exmapleTaskButtonHover "
+                    >
+                        wczytaj przykładowe dane
+                    </button>
+                    </span>
+                </>
+                ) 
+            };
         </div>
     )
 };
