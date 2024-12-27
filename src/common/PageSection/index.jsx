96e0468eef86f36d0ae9c102a802bf1c34bab39e
Jakub Musialowski <mds@mds.biz.pl>
2024-12-27 22:51:04 +0100

    App styling corrected

--- src/common/PageSection/index.jsx
+++ src/common/PageSection/index.jsx
@@ -1,16 +1,16 @@
 const PageSection = ({title, body, headerMenu, menu}) => (
     // <div className="h-full overflow-hidden">
-        <div className="border border-solid border-borderGray h-full">
-        {title ? (
-            <div className="border-b border-solid border-b-borderGray p-3 bg-primaryGreyColor flex items-center sticky">
-                <h2 className="text-lg/[1] font-bold grow">{title}</h2>
-                {headerMenu}
+        <div className="h-full">
+            {title ? (
+                <div className="border border-solid border-borderGray p-3 bg-primaryGreyColor flex items-center sticky">
+                    <h2 className="text-lg/[1] font-bold grow">{title}</h2>
+                    {headerMenu}
+                </div>
+            ) : ""}
+            {menu}
+            <div className="p-3 h-[calc(100%-91px)] overflow-y-auto">
+                {body}
             </div>
-        ) : ""}
-        {menu}
-        <div className="p-3 h-[calc(100%-91px)] overflow-y-auto">
-            {body}
-        </div>
         </div>
     // </div>
 );
