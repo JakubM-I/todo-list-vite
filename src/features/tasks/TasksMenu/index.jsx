44390e322a13da500ad3fff22ee03348da2573ed
Jakub Musialowski <mds@mds.biz.pl>
2024-12-29 19:27:31 +0100

    Task menu styling corrected

--- src/features/tasks/TasksMenu/index.jsx
+++ src/features/tasks/TasksMenu/index.jsx
@@ -12,7 +12,7 @@ const TasksMenu = () => {
     return (
         <>
             {tasks.length > 0 && (<div className="w-[calc(100%-20px)] mx-auto border-x border-b border-borderGray border-solid rounded-b sticky">
-                <ul className="flex flex-wrap justify-center items-center gap-2 p-[5px] pr-2 text-sm mobile-l:justify-end mobile-l:flex-nowrap mobile-l:gap-4">
+                <ul className="flex flex-wrap justify-evenly items-center gap-2 p-[5px] pr-2 text-sm mobile-l:justify-end mobile-l:flex-nowrap mobile-l:gap-4">
                     {isAnyTaskDone && (
                     <li>
                         <button 
