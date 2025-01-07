394351ee920792de0a5069526e8a29d431ff041a
Jakub Musialowski <mds@mds.biz.pl>
2025-01-07 22:32:20 +0100

    Change button element in task page component

--- src/common/PrimaryButton/index.jsx
+++ src/common/PrimaryButton/index.jsx
@@ -1,6 +1,7 @@
-const PrimaryButton = ({title, onClick}) => (
+const PrimaryButton = ({title, onClick, type}) => (
     <button className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue"
     onClick={onClick}
+    type={type}
     >
         {title}
     </button>
