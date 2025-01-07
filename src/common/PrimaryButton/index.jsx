9fa78ad9400867d7798fa08a08499d6672a22042
Jakub Musialowski <mds@mds.biz.pl>
2025-01-07 22:13:42 +0100

    Chnage AddButton component to global button component

--- src/common/PrimaryButton/index.jsx
+++ src/common/PrimaryButton/index.jsx
@@ -1,4 +1,4 @@
-const AddButton = ({title, onClick}) => (
+const PrimaryButton = ({title, onClick}) => (
     <button className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue"
     onClick={onClick}
     >
@@ -6,4 +6,4 @@ const AddButton = ({title, onClick}) => (
     </button>
 );
 
-export default AddButton;
+export default PrimaryButton;
