0ceb320463f5674e9734281ead601287b0006a0e
Jakub Musialowski <mds@mds.biz.pl>
2025-01-07 22:36:36 +0100

    Change button element in task add form

--- src/features/tasks/AddForm/index.jsx
+++ src/features/tasks/AddForm/index.jsx
@@ -101,9 +101,6 @@ const AddTaskForm = () => {
                         title="Dodaj"
                         onClick={onFormSubmit}
                     />
-                    {/* <button
-                        className="bg-primaryBlue rounded-lg border border-primaryBlue text-primaryLightColor text-[min(max(2.89vw,14px),17px)]/[1] py-2 px-3 transition-colors easy-in duration-700 hover:bg-primaryLightColor hover:text-primaryBlue"
-                        onClick={onFormSubmit}>Dodaj</button> */}
                 </div>
             </form>
     )
