779d8e5eb5e228eec2338e1b0dbdd816a00cac94
Jakub Musialowski <mds@mds.biz.pl>
2025-01-10 23:53:59 +0100

    Task page mobile styling corrected

--- src/features/tasks/TaskPage/index.jsx
+++ src/features/tasks/TaskPage/index.jsx
@@ -34,8 +34,8 @@ const TaskPage = () => {
                 onClick={isMobile ? handleContentCLick : undefined}
             >
                 <div>            
-                    <span className="flex justify-end">
-                        <button onClick={() => onClickBack()} className="pr-1 p-[2px] mb-1 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
+                    <span className="flex justify-end pt-1 mobile-t:pt-0">
+                        <button onClick={() => onClickBack()} className="pr-1 p-[2px] mb-1 mr-1 mobile-t:mr-0 border border-primaryLightColor hover:border-solid hover:border-borderGray hover:bg-[#e2e8f052] rounded">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="currentColor" d="M5.146 5.146a.5.5 0 0 1 .708 0L12 11.293l6.146-6.147a.5.5 0 0 1 .638-.057l.07.057a.5.5 0 0 1 0 .708L12.707 12l6.147 6.146a.5.5 0 0 1 .057.638l-.057.07a.5.5 0 0 1-.708 0L12 12.707l-6.146 6.147a.5.5 0 0 1-.638.057l-.07-.057a.5.5 0 0 1 0-.708L11.293 12 5.146 5.854a.5.5 0 0 1-.057-.638z"></path></svg>
                         </button>
                     </span>
