2670f7ddae56d9631b8968b8cc8c5d330420ff99
Jakub Musialowski <mds@mds.biz.pl>
2025-01-28 22:49:33 +0100

    Add next main interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,10 +1,22 @@
 export interface Task {
-    id: string,
-    taskName: string,
-    taskDesc?: string,
-    taskPriority?: string,
-    taskDate?: string,
-    taskCategory?: string,
-    taskDone: false | true,
-    taskVisibility: false | true,
+    id: string;
+    taskName: string;
+    taskDesc?: string;
+    taskPriority?: string;
+    taskDate?: string;
+    taskCategory?: string;
+    taskDone: false | true;
+    taskVisibility: false | true;
+}
+
+export interface Category {
+    categoryId: string;
+    categoryName: string;
+}
+
+export interface Configuration {
+    fetchError: true | false;
+    lang: string;
+    loading: true | false;
+    sortType: string;
 }
