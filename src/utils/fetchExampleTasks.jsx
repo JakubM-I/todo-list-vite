c238e0d216695b5c8d95890ab852fc00d69cc4d0
Jakub Musialowski <mds@mds.biz.pl>
2025-02-02 20:54:32 +0100

    Correct fetch data function

--- src/utils/fetchExampleTasks.jsx
+++
@@ -1,9 +0,0 @@
-export const fetchExampleDataFile = async () => {
-    const response = await fetch("https://todolist-ext-app.netlify.app/exampleData.json");
-
-    if(!response.ok){
-        throw new Error(response.statusText)
-    };
-
-    return await response.json();
-}
--- src/utils/fetchExampleTasks.tsx
+++ src/utils/fetchExampleTasks.tsx
@@ -1,7 +1,7 @@
 import { Task } from "../types/interfaces";
 
 export const fetchExampleDataFile = async (): Promise<Task[]> => {
-    const response = await fetch("/todo-list-vite/exampleData.json");
+    const response = await fetch("https://todolist-ext-app.netlify.app/exampleData.json");
 
     if(!response.ok){
         throw new Error(response.statusText)
