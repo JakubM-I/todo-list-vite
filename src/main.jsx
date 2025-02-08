3d143eae8fb82a9daf75c49aa847e04dc175ba46
Jakub Musialowski <mds@mds.biz.pl>
2025-02-08 23:53:21 +0100

    Change to tsx routes file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from "react-redux";
-import { routes } from "./routes/routes.jsx"
+import { routes } from "./routes/routes.tsx"
 import './index.css'
 import { store } from './store.tsx';
 
