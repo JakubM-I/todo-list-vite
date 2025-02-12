f805cde8cecb26cd9b2920a3a2e7c811eaa14d73
Jakub Musialowski <mds@mds.biz.pl>
2025-02-12 22:40:33 +0100

    Change tasks list wrapper component to tsx

--- src/features/tasks/TasksListWrapper/index.tsx
+++ src/features/tasks/TasksListWrapper/index.tsx
@@ -1,17 +1,18 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
 import { BiArrowBack } from "react-icons/bi"
 import TasksList from "../TaskList"
 import { closeSearchingTasks, isTaskSearching, taskSelector } from "../taskSlice";
 import EmptyTasksList from "../EmptyTasksList";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
+import React from "react";
 
-const TasksListWrapper = () => {
-    const dispatch = useDispatch();
+const TasksListWrapper = (): React.ReactElement => {
+    const dispatch = useAppDispatch();
     const navigate = useNavigate();
-    const tasks = useSelector(taskSelector);
-    const isSearchingOm = useSelector(isTaskSearching);
+    const tasks = useAppSelector(taskSelector);
+    const isSearchingOm = useAppSelector(isTaskSearching);
 
-    const closeSearchTask = () => {
+    const closeSearchTask = (): void => {
         navigate("/");
         dispatch(closeSearchingTasks());
     }
@@ -20,16 +21,16 @@ const TasksListWrapper = () => {
         <>
             {isSearchingOm && (
                 <div className="pb-3">
-                    <button 
+                    <button
                         onClick={() => closeSearchTask()}
                         className="text-sm text-primaryTextColor hover:text-primaryBlueHover flex justify-start items-center gap-2"
-                    > 
+                    >
                         <BiArrowBack />Wróć do wszystkich
                     </button>
                 </div>)
             }
             <div className="flex gap-3">
-                { tasks.length > 0 ? (
+                {tasks.length > 0 ? (
                     <>
                         <TasksList />
                         <Outlet />
