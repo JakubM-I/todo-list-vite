3b705def91e6b5124b1f64e5b483c1225aee7239
Jakub Musialowski <mds@mds.biz.pl>
2025-02-15 23:06:17 +0100

    Change category saga to tsx

--- src/features/categories/categoriesSaga.tsx
+++ src/features/categories/categoriesSaga.tsx
@@ -1,13 +1,14 @@
 import { call, select, takeEvery } from "redux-saga/effects";
 import { categorySelector } from "./categorySlice";
 import { saveCategoryToLocalStorage } from "../../utils/localStorage";
+import { Category } from "../../types/interfaces";
 
-function* saveCategoryToLocalStorageWorker(){
-    const categories = yield select(categorySelector);
+function* saveCategoryToLocalStorageWorker() {
+    const categories: Category = yield select(categorySelector);
 
     yield call(saveCategoryToLocalStorage, categories)
 };
 
-export function* categoriesSaga(){
+export function* categoriesSaga() {
     yield takeEvery("*", saveCategoryToLocalStorageWorker);
 }
