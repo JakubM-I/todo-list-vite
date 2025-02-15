7b3ea389bfc89e32efe42c690c4fd21b43ee09a8
Jakub Musialowski <mds@mds.biz.pl>
2025-02-15 23:32:01 +0100

    Correct types in fetch example data file function

--- src/utils/fetchExampleTasks.tsx
+++ src/utils/fetchExampleTasks.tsx
@@ -1,11 +1,11 @@
-import { Task } from "../types/interfaces";
+import { ExampleData } from "../types/interfaces";
 
-export const fetchExampleDataFile = async (): Promise<Task[]> => {
+export const fetchExampleDataFile = async (): Promise<ExampleData[]> => {
     const response = await fetch("/todo-list-vite/exampleData.json");
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error(response.statusText)
     };
 
-    return await response.json() as Task[];
+    return await response.json() as ExampleData[];
 }
